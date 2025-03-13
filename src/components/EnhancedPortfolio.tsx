283f245d541c431edef68d45f7308028fd9cb1bd
SyedNazmusSakib-SNS <syednazmussakib64@gmail.com>
2025-03-14 05:19:02 +0600

    resume_updtaed

--- src/components/EnhancedPortfolio.tsx
+++ src/components/EnhancedPortfolio.tsx
@@ -305,7 +305,7 @@ export default function EnhancedPortfolio() {
             ))}
             <li className="md:ml-4">
               <motion.a
-                href="https://drive.google.com/file/d/1qMK3YlnAMAffMHte6N-0l-8L1VOMIVuC/view?usp=sharing"
+                href="https://drive.google.com/file/d/1T0eR7Benpv5dyz-xAVHjOlDKgjegMEKO/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.05 }}
@@ -586,7 +586,7 @@ export default function EnhancedPortfolio() {
                     Get in touch
                   </motion.a>
                   <motion.a
-                    href="https://drive.google.com/file/d/1qMK3YlnAMAffMHte6N-0l-8L1VOMIVuC/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1T0eR7Benpv5dyz-xAVHjOlDKgjegMEKO/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     whileHover={{ scale: 1.05 }}
